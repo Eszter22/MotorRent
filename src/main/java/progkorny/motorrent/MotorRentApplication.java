b2be2ef887ad9769c24fb51967a84cba0d7cd815
Eszter22 <esztii20020806@gmail.com>
2025-06-22 19:46:53 +0200

    start

---
+++ src/main/java/progkorny/motorrent/MotorRentApplication.java
@@ -0,0 +1,13 @@
+package progkorny.motorrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotorRentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotorRentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/progkorny/motorrent/MotorRentApplicationTests.java
@@ -0,0 +1,13 @@
+package progkorny.motorrent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotorRentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
