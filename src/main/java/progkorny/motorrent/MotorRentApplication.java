a699be6a03fef43c86b04ea3dc6b00abbee51262
Eszter22 <esztii20020806@gmail.com>
2025-06-22 21:33:05 +0200

    commit

--- src/main/java/progkorny/motorrent/MotorRentApplication.java
+++ src/main/java/progkorny/motorrent/MotorRentApplication.java
@@ -3,9 +3,11 @@ package progkorny.motorrent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication// Ez az annotáció jelzi, hogy ez egy Spring Boot alkalmazás, és engedélyezi az automatikus konfigurációt, komponens-keresést, stb.
 public class MotorRentApplication {
-
+	// A SpringApplication.run elindítja a Spring Boot alkalmazást,
+	// betölti a kontextust, inicializálja a bean-eket, és elindítja a beágyazott szervert (pl. Tomcat),
+	// hogy a webalkalmazás elérhető legyen.
 	public static void main(String[] args) {
 		SpringApplication.run(MotorRentApplication.class, args);
 	}
